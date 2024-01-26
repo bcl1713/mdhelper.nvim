274bac69ab03de45dd68a06b2cc5c72b6356af29
Brian Lucas <bcl1713@gmail.com>
2024-01-26 16:14:59 +0100

    feat: map on markdown handle <cr>

--- plugin/mdhelper.lua
+++ plugin/mdhelper.lua
@@ -1,3 +1,26 @@
 if vim.fn.has("nvim-0.7.0") ~= 1 then
   vim.api.nvim_err_writeln("mdhelper.nvim requires at least nvim-0.7.0")
 end
+
+local function handle_lists()
+  vim.api.nvim_feedkeys("\n", "n", true)
+  print("Hello")
+end
+
+local enabled = true
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    local data = {
+      buf = vim.fn.expand("<abuf>"),
+      file = vim.fn.expand("<aflile>"),
+      match = vim.fn.expand("<amatch>"),
+    }
+    if enabled then
+      vim.keymap.set("i", "<cr>", function()
+        handle_lists()
+      end, { buffer = tonumber(data.buf), noremap = true })
+    end
+  end,
+})
