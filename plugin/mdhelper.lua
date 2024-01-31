a11380d95f8f64c3fd9f4c4b2ff96445242f306b
Brian Lucas <bcl1713@gmail.com>
2024-01-31 15:57:02 +0100

    feat: auto insert todo items and lists

--- plugin/mdhelper.lua
+++ plugin/mdhelper.lua
@@ -1,26 +1,31 @@
 if vim.fn.has("nvim-0.7.0") ~= 1 then
-  vim.api.nvim_err_writeln("mdhelper.nvim requires at least nvim-0.7.0")
+	vim.api.nvim_err_writeln("mdhelper.nvim requires at least nvim-0.7.0")
 end
 
 local function handle_lists()
-  vim.api.nvim_feedkeys("\n", "n", true)
-  print("Hello")
+	local current_line = vim.api.nvim_get_current_line()
+	vim.api.nvim_feedkeys("\n", "n", true)
+	if string.find(current_line, "- [ ]", 0, true) or string.find(current_line, "- [x]", 0, true) then
+		vim.api.nvim_feedkeys("- [ ] ", "n", true)
+	elseif string.find(current_line, "- ", 0, true) then
+		vim.api.nvim_feedkeys("- ", "n", true)
+	end
 end
 
 local enabled = true
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    local data = {
-      buf = vim.fn.expand("<abuf>"),
-      file = vim.fn.expand("<aflile>"),
-      match = vim.fn.expand("<amatch>"),
-    }
-    if enabled then
-      vim.keymap.set("i", "<cr>", function()
-        handle_lists()
-      end, { buffer = tonumber(data.buf), noremap = true })
-    end
-  end,
+	pattern = "markdown",
+	callback = function()
+		local data = {
+			buf = vim.fn.expand("<abuf>"),
+			file = vim.fn.expand("<aflile>"),
+			match = vim.fn.expand("<amatch>"),
+		}
+		if enabled then
+			vim.keymap.set("i", "<cr>", function()
+				handle_lists()
+			end, { buffer = tonumber(data.buf), noremap = true })
+		end
+	end,
 })
